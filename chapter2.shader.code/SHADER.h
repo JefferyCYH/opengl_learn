6331657f2a1ff1fd1546ff68f96f826fd35dd79e
JefferyCYH <siwangzhishen30@qq.com>
2020-12-02 11:00:38 +0800

    Create SHADER.h

---
+++ chapter2.shader.code/SHADER.h
@@ -0,0 +1,34 @@
+#pragma once
+#define GLEW_STATIC
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include<string>
+using namespace std;
+#define GLEW_STATIC
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+class Shader
+{
+public:
+    // shader程序ID
+    unsigned int ID;
+
+    /*string vertexString;
+    string fragmentString;
+    const char* vertexSource;
+    const char* fragmentSource;*/
+
+
+    // 构造器读取并构建着色器
+    Shader(const char* vertexPath, const char* fragmentPath);
+    // 使用/激活程序
+    void use();
+    // uniform工具函数
+    void setBool(const std::string& name, bool value) const;
+    void setInt(const std::string& name, int value) const;
+    void setFloat(const std::string& name, float value) const;
+};
